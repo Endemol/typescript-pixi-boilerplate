648925627e92c7441e9f3931a1a0ca5c7b7bdba9
Steven Archer <email@stevenarcher.com>
2016-03-09 18:09:50 +0000

    init project files

---
+++ README.md
@@ -0,0 +1,16 @@
+# PixiJS Font Test
+
+
+## Requirements
+* [Node.js](http://nodejs.org/) - an asynchronous event driven javascript framework
+* [NPM](https://www.npmjs.com/) - a package manager for javascript.
+```
+$ sudo npm install npm -g.
+$ Test: Run npm -v. The version should be higher than 2.1.8.
+```
+
+* [TypeScript](http://www.typescriptlang.org/) - a typed superset of JavaScript that compiles to plain JavaScript
+
+```
+$ npm install -g typescript
+```
---
+++ gulpfile.js
@@ -0,0 +1,236 @@
+//	 ____        _ _     _   ____            _       _
+//	| __ ) _   _(_) | __| | / ___|  ___ _ __(_)_ __ | |_
+//	|  _ \| | | | | |/ _` | \___ \ / __| '__| | '_ \| __|
+//	| |_) | |_| | | | (_| |  ___) | (__| |  | | |_) | |_
+//	|____/ \__,_|_|_|\__,_| |____/ \___|_|  |_| .__/ \__|
+//										      |_|
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// Gulp Libraries
+// --------------------------------------------------------------------------------------------------------------------
+
+var gulp = require('gulp');                             // Gulp
+var browserSync = require('browser-sync');              // Open a new browser session and load changes instantly
+var del = require('del');                               // Delete files
+var runSequence = require('run-sequence');              // Manage async tasks
+var changed = require('gulp-changed');                  // Only affect changed files
+var sourcemaps = require('gulp-sourcemaps');            // Source Maps
+var watch = require('gulp-watch');                      // Watch
+var concat = require('gulp-concat');                    // For Combining JavaScript files
+var uglify = require('gulp-uglify');                    // For minifing Javascript
+var debug = require('gulp-debug');						// Streams to see what files are run through your gulp pipeline
+var	tsc = require('gulp-typescript');					// TypeScript Compiler
+var	tslint = require('gulp-tslint');					// TypeScript Linter
+var prompt = require('gulp-prompt');                    // Interactive console prompts
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// Directory
+// --------------------------------------------------------------------------------------------------------------------
+
+
+var dir = {source: {}, library: {}, build: {}, assets: {}, binary: {}};
+
+/**
+ * Get Directories
+ * @param {String} language
+ */
+function getDirectory(language) {
+
+	// SOURCE -------------------------------------------------
+	dir.source.root 	= 'source';
+	dir.source.js 		= dir.source.root + '/js';
+	dir.source.json 	= dir.source.root + '/json/'+language;
+	dir.source.ts 		= dir.source.root + '/ts';
+	dir.source.sass 	= dir.source.root + '/sass';
+	dir.source.html 	= dir.source.root + '/html';
+	dir.source.css 		= dir.source.root + '/css';
+
+	// LIBRARY ------------------------------------------------
+	dir.library.bower 	= 'bower_components';
+	dir.library.npm 	= 'node_modules';
+
+	// ASSETS -------------------------------------------------
+	dir.assets.root 	= 'assets';
+	dir.assets.images 	= dir.assets.root + '/images';
+	dir.assets.fonts 	= dir.assets.root + '/fonts/'+language;
+	dir.assets.sounds 	= dir.assets.root + '/sounds';
+
+	// BUILD --------------------------------------------------
+	dir.build.root 		= 'build';
+	dir.build.js 		= dir.build.root + '/js/';
+	dir.build.json 		= dir.build.root + '/json/';
+	dir.build.css 		= dir.build.root + '/css/';
+	dir.build.images 	= dir.build.root + '/res/';
+	dir.build.fonts 	= dir.build.root + '/lang/'+language+'/';
+	dir.build.json 		= dir.build.root + '/lang/'+language+'/';
+	dir.build.sounds 	= dir.build.root + '/sounds/';
+	dir.build.html 		= dir.build.root;
+}
+getDirectory('en');
+
+// --------------------------------------------------------------------------------------------------------------------
+// Constants
+// --------------------------------------------------------------------------------------------------------------------
+var deploymentID = {
+	DEV:0,
+	STAGING:1,
+	PRODUCTION:2
+};
+
+var operatorID = {
+	BLUEPRINT:0
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+// Utils
+// --------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Console Colours
+ */
+var cc = {
+	cyan:'\033[0;36m',
+	yellow:'\033[0;33m',
+	pink:'\033[0;35m',
+	clear:'\033[0m'
+};
+
+/**
+ * Report Change
+ * @param {{path:String, type:String}} event
+ */
+function reportChange(event){
+	console.log('File ' + cc.pink + event.path + cc.clear + ' was ' + cc.cyan + event.type + cc.clear + ' running tasks...');
+};
+
+/**
+ * Error Handler
+ * @param error
+ */
+function errorHandler(error){
+	console.log('Error',error.message);
+};
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// Gulp Tasks
+// --------------------------------------------------------------------------------------------------------------------
+
+// CSS ----------------------------------------------------
+gulp.task('css', function () {
+	return gulp.src([dir.source.css+'/**/*'])
+		.pipe(changed(dir.build.css))
+		.pipe(gulp.dest(dir.build.css))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// HTML ---------------------------------------------------
+gulp.task('html', function () {
+	return gulp.src([dir.source.html+'/**/*'])
+		.pipe(changed(dir.build.html))
+		.pipe(gulp.dest(dir.build.html))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// FONTS --------------------------------------------------
+gulp.task('fonts', function () {
+	return gulp.src(dir.assets.fonts+'/**/*')
+		.pipe(changed(dir.build.fonts))
+		.pipe(gulp.dest(dir.build.fonts))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// IMAGES -------------------------------------------------
+gulp.task('images', function () {
+	return gulp.src([dir.assets.images+'/**/*'])
+		.pipe(changed(dir.build.images))
+		.pipe(gulp.dest(dir.build.images))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// SOUNDS -------------------------------------------------
+gulp.task('sounds', function () {
+	return gulp.src([dir.assets.sounds+'/**/*'])
+		.pipe(changed(dir.build.sounds))
+		.pipe(gulp.dest(dir.build.sounds))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// JSON ---------------------------------------------------
+gulp.task('json', function () {
+	return gulp.src(dir.source.json+'/**/*.json')
+		.pipe(changed(dir.build.json))
+		.pipe(gulp.dest(dir.build.json))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// JAVASCRIPT  --------------------------------------------
+gulp.task('js', function () {
+	return gulp.src(dir.source.js+'/**/*')
+		.pipe(changed(dir.build.js))
+		.pipe(gulp.dest(dir.build.js))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// TYPESCRIPT ---------------------------------------------
+gulp.task('ts', function () {
+	return gulp.src(dir.source.ts+'/**/*')
+		.pipe(sourcemaps.init())
+		.pipe(tsc({target:'ES5'}))
+		.pipe(concat('App.js'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(dir.build.js));
+});
+
+// CLEAN --------------------------------------------------
+gulp.task('clean', function() {
+	return del(dir.build.root, { force: true });
+});
+
+// WATCH --------------------------------------------------
+gulp.task('watch', [], function() {
+	gulp.watch(dir.source.ts+'/**/*.css', ['css']).on('change', reportChange);
+	gulp.watch(dir.source.ts+'/**/*.ts', ['ts']).on('change', reportChange);
+	gulp.watch(dir.source.js+'/**/*', ['js']).on('change', reportChange);
+	gulp.watch(dir.source.html+'/**/*', ['html']).on('change', reportChange);
+	gulp.watch(dir.source.images+'/**/*', ['images']).on('change', reportChange);
+});
+
+// BROWSER SYNC -------------------------------------------
+gulp.task('serve', function(done) {
+	browserSync({
+		open: true,
+		port: 9090,
+		server: {
+			baseDir: [dir.build.root],
+			directory: true,
+			middleware: function (req, res, next) {
+				res.setHeader('Access-Control-Allow-Origin', '*');
+				next();
+			}
+		}
+	}, done);
+});
+
+
+// INIT ---------------------------------------------------
+gulp.task('init', function () {
+
+});
+
+
+// DEFAULT ------------------------------------------------
+gulp.task('default', function () {
+	runSequence(
+		'clean',
+		'ts',
+		['js','json','images','css','html','fonts','sounds'],
+		'watch',
+		'serve'
+	);
+});
+
+
