7868609bad305b693aa85bc257ae027e303fb046
Frank McPolin <frank.mcpolin@endemolshinegaming.com>
2019-09-17 08:13:52 +0100

    Adding setup-project

---
+++ setup-project.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+ECHO SLOT Batch Script Setting up project
+ECHO Installing NPM packages...
+cmd /C npm install
+
+REM Installing gulp-cli if not done already
+SET OUTPUT=""
+FOR /F "tokens=*" %%a in ('npm list -g ^| find /n "gulp-cli"') do SET OUTPUT=%%a
+IF "%OUTPUT%" == """" (
+	ECHO Installing gulp-cli...
+	cmd /C npm install -g gulp-cli
+)
+
+REM Installing typescript if not done already
+SET OUTPUT=""
+FOR /F "tokens=*" %%a in ('npm list -g ^| find /n "typescript"') do SET OUTPUT=%%a
+IF "%OUTPUT%" == """" (
+	ECHO Installing Typescript...
+	cmd /C npm install -g typescript
+)
+
+ECHO Linking TypeScript...
+cmd /C npm link typescript
+ECHO Initialising Git submodules...
+cmd /C git submodule update --init --recursive
+ECHO Creating directories...
+cmd /C MKDIR \build
+ECHO Initialising Gulp...
+cmd /C gulp init
+
---
+++ setup-project.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+red='\033[0;31m'
+white='\033[1;37m'
+cc='\033[0m' # Clear Color
+echo -e "Shell Script ${red}Setting up project${cc}"
+echo -e "${white}Installing${cc} NPM packages..."
+npm install
+
+# Installing globally gulp-cli if not done already
+if [[ -z $(npm list -g | grep gulp-cli) ]]
+then
+	echo -e "${white}Installing${cc} gulp-cli..."
+	npm install -g gulp-cli
+fi
+
+# Installing globally typescript if not done already
+if [[ -z $(npm list -g | grep typescript) ]]
+then
+	echo -e "${white}Installing${cc} TypeScript..."
+	npm install -g typescript
+fi
+
+echo -e "${white}Linking${cc} TypeScript..."
+npm link typescript
+echo -e "${white}Initialising${cc} Git submodules..."
+git submodule update --init --recursive
+echo -e "${white}Creating${cc} directories..."
+mkdir build/
+echo -e "${white}Initialising${cc} Gulp..."
+gulp init
